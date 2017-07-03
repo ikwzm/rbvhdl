2ce449831dcfe9753ea1c7d5ba819415867d22e5
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2017-07-03 09:39:01 +0900

    [add] method _elsif! and _else! to RbVHDL::Ast::Statement::If

--- spec/rbvhdl/writer/statement/if_spec.rb
+++ spec/rbvhdl/writer/statement/if_spec.rb
@@ -5,12 +5,15 @@ describe 'RbVHDL::Ast::Statement::If' do
   context "_write_line" do
 
     it "if_statement(RbVHDL::Ast.name('cond_0'))" do
-      design_unit = RbVHDL::Ast::DesignUnit.new
-      body = design_unit._architecture('MODEL', 'TEST')
-      proc = body._process_statement
-      stmt = proc._if_statement(RbVHDL::Ast.name('cond_0'))
+      design_unit = RbVHDL::Ast::DesignUnit.new {
+        _architecture('MODEL', 'TEST') {
+          _process_statement {
+            _if_statement(RbVHDL::Ast.name('cond_0'))
+          }
+        }
+      }
 
-      line = body._write_line
+      line = design_unit._write_line
       expect(line.shift).to eq "architecture MODEL of TEST is"
       expect(line.shift).to eq "begin"
       expect(line.shift).to eq "    process begin"
@@ -21,12 +24,15 @@ describe 'RbVHDL::Ast::Statement::If' do
     end
 
     it "if_statement(RbVHDL::Ast.name('cond_0'))._label!('IF_COND0')" do
-      design_unit = RbVHDL::Ast::DesignUnit.new
-      body = design_unit._architecture('MODEL', 'TEST')
-      proc = body._process_statement
-      stmt = proc._if_statement(RbVHDL::Ast.name('cond_0'))._label!('IF_COND0')
+      design_unit = RbVHDL::Ast::DesignUnit.new {
+        _architecture('MODEL', 'TEST'){
+          _process_statement {
+            _if_statement(RbVHDL::Ast.name('cond_0'))._label!('IF_COND0')
+          }
+        }
+      }
 
-      line = body._write_line
+      line = design_unit._write_line
       expect(line.shift).to eq "architecture MODEL of TEST is"
       expect(line.shift).to eq "begin"
       expect(line.shift).to eq "    process begin"
@@ -40,15 +46,19 @@ describe 'RbVHDL::Ast::Statement::If' do
   context "_elsif_statement and _else_statement" do
 
     it "if_statement(name('cond_0')) ... _elsif_statement(name('cond_1'))" do
-      design_unit = RbVHDL::Ast::DesignUnit.new
-      body = design_unit._architecture('MODEL', 'TEST')
-      proc = body._process_statement
-      if_stmt = proc._if_statement(RbVHDL::Ast.name('cond_0'))
-      if_stmt._null_statement
-      e0_stmt = if_stmt._elsif_statement(RbVHDL::Ast.name('cond_1'))
-      e0_stmt._null_statement
+      design_unit = RbVHDL::Ast::DesignUnit.new{
+        _architecture('MODEL', 'TEST') {
+          _process_statement {
+            _if_statement(RbVHDL::Ast.name('cond_0')) {
+              _null_statement
+            }._elsif_statement(RbVHDL::Ast.name('cond_1')) {
+              _null_statement
+            }
+          }
+        }
+      }
       
-      line = body._write_line
+      line = design_unit._write_line
       expect(line.shift).to eq "architecture MODEL of TEST is"
       expect(line.shift).to eq "begin"
       expect(line.shift).to eq "    process begin"
@@ -62,15 +72,19 @@ describe 'RbVHDL::Ast::Statement::If' do
     end
 
     it "if_statement(name('cond_0')) ... _else_statement" do
-      design_unit = RbVHDL::Ast::DesignUnit.new
-      body = design_unit._architecture('MODEL', 'TEST')
-      proc = body._process_statement
-      if_stmt = proc._if_statement(RbVHDL::Ast.name('cond_0'))
-      if_stmt._null_statement
-      e0_stmt = if_stmt._else_statement
-      e0_stmt._null_statement
+      design_unit = RbVHDL::Ast::DesignUnit.new {
+        _architecture('MODEL', 'TEST'){
+          _process_statement {
+            _if_statement(RbVHDL::Ast.name('cond_0')) {
+              _null_statement
+            }._else_statement {
+              _null_statement
+            }
+          }
+        }
+      }
 
-      line = body._write_line
+      line = design_unit._write_line
       expect(line.shift).to eq "architecture MODEL of TEST is"
       expect(line.shift).to eq "begin"
       expect(line.shift).to eq "    process begin"
@@ -82,28 +96,35 @@ describe 'RbVHDL::Ast::Statement::If' do
       expect(line.shift).to eq "    end process;"
       expect(line.shift).to eq "end MODEL;"
     end
+  end
+  
+  context "_elsif! and _else!" do
 
-    it "if_statement(name('cond_0')) ... _elsif_statement(name('cond_1')) ... _else_statement" do
-      design_unit = RbVHDL::Ast::DesignUnit.new
-      body = design_unit._architecture('MODEL', 'TEST')
-      proc = body._process_statement
-      if_stmt = proc._if_statement(RbVHDL::Ast.name('cond_0'))
-      if_stmt._null_statement
-      e0_stmt = if_stmt._elsif_statement(RbVHDL::Ast.name('cond_1'))
-      e0_stmt._null_statement
-      e1_stmt = if_stmt._else_statement
-      e1_stmt._null_statement
+    it "if_statement(name('cond_0')){...}._elsif!(name('cond_1')){...}._else!{...}" do
+      design_unit = RbVHDL::Ast::DesignUnit.new {
+        _architecture('MODEL', 'TEST') {
+          _process_statement {
+            _if_statement(RbVHDL::Ast.name('cond_0')) {
+              _null_statement._label!("NULL_IF_COND_0")
+            }._elsif!(RbVHDL::Ast.name('cond_1')) {
+              _null_statement._label!("NULL_IF_COND_1")
+            }._else! {
+              _null_statement._label!("NULL_ELSE")
+            }
+          }
+        }
+      }
 
-      line = body._write_line
+      line = design_unit._write_line
       expect(line.shift).to eq "architecture MODEL of TEST is"
       expect(line.shift).to eq "begin"
       expect(line.shift).to eq "    process begin"
       expect(line.shift).to eq "        if    cond_0 then"
-      expect(line.shift).to eq "            null;"
+      expect(line.shift).to eq "            NULL_IF_COND_0: null;"
       expect(line.shift).to eq "        elsif cond_1 then"
-      expect(line.shift).to eq "            null;"
+      expect(line.shift).to eq "            NULL_IF_COND_1: null;"
       expect(line.shift).to eq "        else"
-      expect(line.shift).to eq "            null;"
+      expect(line.shift).to eq "            NULL_ELSE: null;"
       expect(line.shift).to eq "        end if;"
       expect(line.shift).to eq "    end process;"
       expect(line.shift).to eq "end MODEL;"
